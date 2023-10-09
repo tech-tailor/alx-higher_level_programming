8e53cf1ab902bc4127a19fecf589f63cf1b4765b
Akin <ogundareakinniyi8@gmail.com>
2023-10-09 23:52:40 +0100

    updates

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
-    my_list.remove(my_list[idx])
-    return my_list
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
