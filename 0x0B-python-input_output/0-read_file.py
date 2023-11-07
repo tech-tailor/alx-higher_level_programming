29d8a86016e1ebbc4239f510ddc161655ff03e3e
Akin <ogundareakinniyi8@gmail.com>
2023-11-07 17:03:07 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+Module for the def read_file(filename=""): function that reads a text file
+(UTF8) and prints it to stdout:
+"""
+
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding="utf-8") as file:
-        for line in file:
-            print(line, end='')
+    """
+    Function that reads a text file (UTF8) and prints it to stdout:
+    """
+    with open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            print(line, end="")
