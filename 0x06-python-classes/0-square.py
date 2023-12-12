2609940ee39c8aa2eb201fb877bae5f6762369f8
Akin <ogundareakinniyi8@gmail.com>
2023-12-12 08:04:27 +0100

    0-square.py updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python3
+#!/usrOB/bin/python3
+"""
+Module: Square module
+
+this module defines a simple Square class for representing square
+
+classes:
+- Square: A class representing a square
+
+Example:
+    >>> my_square = Square()
+    >>> print(my_square.__dict__)
+    {}
+"""
 class Square:
     """
     Represent a square
