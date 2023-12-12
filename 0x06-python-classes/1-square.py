20c3bcfbbaf5c924221eb6d3d1859d7718e1dd3b
Akin <ogundareakinniyi8@gmail.com>
2023-12-12 09:41:54 +0100

    updated 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Module: square_module
+
+This module defines a simple class for representing square
+
+Classes:
+- Square: A class representing a square
+"""
+
+
 class Square:
     """
     A class that define a square
