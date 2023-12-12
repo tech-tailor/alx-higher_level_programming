a6fdbc1dd2545f703e4819ce97583d94c7b87dca
Akin <ogundareakinniyi8@gmail.com>
2023-12-12 14:38:12 +0100

    0-rectangle.py  readme updated

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: Rectangle module
+
+this module defines a simple class for representing rectangle
+
+classes:
+- Rectangle: A class representing rectangle
+"""
+
+class Rectangle:
+    """
+    this is a class called rectangle
+    it has no attributes
+    it has no methods
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+readme for 0x08. Python - More Classes and Objects
