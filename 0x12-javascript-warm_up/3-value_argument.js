103120fd2c855775c2779219b11293a6a7f90769
Akin <ogundareakinniyi8@gmail.com>
2024-01-09 11:56:16 +0100

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -8,7 +8,5 @@ args.forEach(arg => {
 if (allArgs === 0) {
   console.log('No argument');
 } else {
-  args.forEach(arg => {
-    console.log(arg);
-  });
+  console.log(process.argv.slice(2).join(','));
 }
