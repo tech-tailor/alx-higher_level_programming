17c21f3034e318ab767ab8be3f66a6b89440c386
Akin <ogundareakinniyi8@gmail.com>
2024-01-13 03:29:48 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" This module to check database
+"""
+
+
+import sys
+import MySQLdb
+
+
+def list_state(user, password, database, state):
+    """This function list a table
+        from th database
+    """
+
+    db = MySQLdb.connect(
+        user=user,
+        password=password,
+        database=database
+        )
+    cursor = db.cursor()
+
+    myQuery = f"SELECT * FROM states WHERE name = '{state}' ORDER BY id;"
+
+    cursor.execute(myQuery)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    user, password, database, state = sys.argv[1:]
+    list_state(user, password, database, state)
