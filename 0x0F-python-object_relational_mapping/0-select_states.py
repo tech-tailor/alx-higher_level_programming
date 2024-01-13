25e138c4a456fd881af1034a764cffd0f5dc1ac6
Akin <ogundareakinniyi8@gmail.com>
2024-01-13 02:30:15 +0100

    add 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" This module to check database
+"""
+
+
+import sys
+import MySQLdb
+
+
+def list_state(user, password, database):
+    """This function list a table
+        from th database
+    """
+
+    db = MySQLdb.connect(
+        user=user,
+        password=password,
+        database=database
+        )
+    cursor = db.cursor()
+
+    myQuery = "SELECT * FROM states ORDER BY id"
+
+    cursor.execute(myQuery)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    user, password, database = sys.argv[1:4]
+    list_state(user, password, database)
