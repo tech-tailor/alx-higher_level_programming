7979862fbf49de5b8c262ee01fc1183f5797f4f4
Akin <ogundareakinniyi@gmail.com>
2024-01-27 17:17:40 +0100

    add 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script to take url, send request
+to the url and display the value
+of X-Request-Id found in the
+header
+"""
+import urllib.request
+import sys
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data ={'email':  sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
