ca10a8c7b36daf39dabeaf19ce5ac8dd11430197
Akin <ogundareakinniyi8@gmail.com>
2024-02-20 16:32:45 +0100

    added 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Read and print a file
+
+const request = require('request');
+
+// get the file path
+const url = process.argv[2];
+
+// Read file content
+request(
+  {
+    method: 'GET',
+    uri: url,
+    encoding: 'utf-8'
+  },
+  (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Code:', response.statusCode);
+    }
+  });
